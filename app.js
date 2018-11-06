78330027e6ec249e994cb29480b903acde4d8593
Maya Messinger <maya.messinger@duke.edu>
2018-11-06 17:14:09 +0000

    Initial setup for connection to PSQL server

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3001;
+var pg = require('pg');
+require('dotenv').config();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var connectionString = "postgres://" + process.env.PSQL_USERNAME + ":" + process.env.PSQL_PASSWORD + "@serverName/" + process.env.PSQL_IP + ":5432/ldb";
+var pgClient = new pg.Client(connectionString);
 
-app.listen(port, () => console.log(`Server running on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Server running on port ${port}!`));
