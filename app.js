0e846a05cd5e432447ef357fc369ea70bb664a96
Maya Messinger <maya.messinger@duke.edu>
2018-11-07 19:58:08 +0000

    Have connection to database seworking, now to connect to front-end

--- app.js
+++ app.js
@@ -4,9 +4,26 @@ const port = 3001;
 var pg = require('pg');
 require('dotenv').config();
 
-var connectionString = "postgres://" + process.env.PSQL_USERNAME + ":" + process.env.PSQL_PASSWORD + "@serverName/" + process.env.PSQL_IP + ":5432/ldb";
-var pgClient = new pg.Client(connectionString);
+var connectionString = 'postgresql://' + process.env.PSQL_USERNAME + ':' + process.env.PSQL_PASSWORD +
+							'@' + process.env.PSQL_IP + ':' + process.env.PSQL_SOCKET + '/' + process.env.PSQL_DB;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+var pgClient = new pg.Client({
+  connectionString: connectionString
+});
+pgClient.connect();
+
+pgClient.query('SELECT * FROM ' + 'Books' + ' LIMIT(100);', (err, res) => {
+  if (err)	{
+  	console.log(err.stack);
+  }
+  else	{
+  	console.log(res.rows[0]);	// .uid, .title, etc. to access columns
+  }
+});
+
+app.get('/', (req, res) => {
+	// handle get requests
+	// if tree to see what the query is asking for
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}!`));
